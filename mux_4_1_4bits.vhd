c4e61baf3efe92683e8d1cd6a9747b047b65e0a0
Andrés Nazarith <164280426+StiffFanatic@users.noreply.github.com>
2025-09-04 16:39:24 -0500

    Update mux_4_1_4bits.vhd

--- mux_4_1_4bits.vhd
+++ mux_4_1_4bits.vhd
@@ -1,33 +1,29 @@
-
 library ieee;
 use ieee.std_logic_1164.all;
 
-
--- este es el multiplexor 4 - 1
-
 entity mux_4_1_4bits is
 
-	 -- el I0 es la Not(A), I1 para A-B, I2 el comparador, I3 para A+B
-	 
-	port(I	:	in std_logic_vector(3 downto 0);		-- 4 entradas
-		  S	:  in std_logic_vector(1 downto 0);		-- selector
-		  Y	:  out std_logic);							-- 1 salida
-					
-		  
+	-- I0 = Not(A), I1 = A-B, I2 = Comparador, I3 = A+B
+	port(
+	
+	
+		I0, I1, I2, I3 : in  std_logic_vector(3 downto 0); -- 4 entradas de 4 bits
+		S              : in  std_logic_vector(1 downto 0); -- selector
+		Y              : out std_logic_vector(3 downto 0)  -- salida de 4 bits
+		
+		
+	);
 end mux_4_1_4bits;
 
-
 architecture behavioral of mux_4_1_4bits is
-
-	begin
-
-		with S select	-- logica del mux
+		begin
 		
-		Y <= I(0) when "00",
-			  I(1) when "01",
-			  I(2) when "10",
-			  I(3) when "11",
-			  '0'  when others;
-  
-
+			with S select
+			
+			Y <= I0 when "00",
+				  I1 when "01",
+				  I2 when "10",
+				  I3 when "11",
+					"0000" when others;
+					
 end behavioral;
